98c2303eac8bdeed15dc5aa087b06185f394c21d
JiaHong531 <omgmvpisme@gmail.com>
2025-10-27 16:47:11 +0800

    Application GUI 1st template

--- SmartToDoApp/src/main/java/com/example/smarttodoapp/MainApplication.java
+++ SmartToDoApp/src/main/java/com/example/smarttodoapp/MainApplication.java
@@ -12,7 +12,7 @@ public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("MainWindow.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 800);
         stage.setTitle("Smart To Do Application");
         try {
             InputStream iconStream = getClass().getResourceAsStream("/images/icon.png");
